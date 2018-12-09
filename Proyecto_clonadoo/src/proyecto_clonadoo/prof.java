a512325562ffe7264802561f706f9bce0e8a6187
PaolaPaniagua <silvia.paola.paniagua@gmail.com>
2018-12-09 12:31:51 -0300

    clase nueva prof

---
+++ Proyecto_clonadoo/src/proyecto_clonadoo/prof.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_clonadoo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class prof {
+ String materia;
+
+    public String getMateria() {
+        return materia;
+    }
+ 
+}
